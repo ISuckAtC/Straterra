574f68dbbe78688a367a64ad605a77e5490ba518
Chandler <345982@student.nord.no>
2023-03-08 11:09:33 +0100

    Exclamation Updated

--- Assets/ColorBlink.cs
+++ Assets/ColorBlink.cs
@@ -1,22 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ColorBlink : MonoBehaviour
 {
     public float speed = 10;
     private TMPro.TMP_Text text;
+    private Image exclamationPoint;
     // Start is called before the first frame update
     void Start()
     {
         text = GetComponent<TMPro.TMP_Text>();
+        exclamationPoint = GetComponent<Image>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Color color = text.color;
+        Color color = exclamationPoint.color;
         color.a = Mathf.PingPong(Time.time * speed, 1);
-        text.color = color;
+        exclamationPoint.color = color;
     }
 }
