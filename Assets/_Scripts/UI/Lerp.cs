5144c744acb576bf1732134e26eedc85017423a9
Mikkel <mikkelking97@gmail.com>
2023-04-14 15:23:04 +0200

    Tutorial Guiding arrow in progress.

--- Assets/_Scripts/UI/Lerp.cs
+++ Assets/_Scripts/UI/Lerp.cs
@@ -10,23 +10,55 @@ public class Lerp : MonoBehaviour
     public float speed = 1f;
     bool tutorialClicked0 = false;
     public BottomBar bottomBar;
-                                                                        
+    public Vector3 currentEulerAngles;
+    public Quaternion currentRotation;
 
+    Transform currentPos;
+    Transform moveToPos;
+
+    private void Start()
+    {
+        currentPos = positionToMoveToA;
+        moveToPos = positionToMoveToB;
+
+
+    }
     public void Update()
     {
-      
+
         scalar += Time.deltaTime * speed;
-        if(!tutorialClicked0)
-        transform.position = Vector3.Lerp(positionToMoveToA.position, positionToMoveToB.position, Mathf.PingPong(scalar, 1f));
+        //if(!bottomBar.worldView)
+        transform.position = Vector3.Lerp(currentPos.position, moveToPos.position, Mathf.PingPong(scalar, 1f));
 
         //In map
         //Find the tile with the highest resource gain of given type.
-        Vector2 tilePosition = Grid._instance.GetPosition(Grid._instance.FindTileWithHighestResourceAmount("food"));
+
+
+        int highestAmount = Grid._instance.FindTileWithHighestResourceAmount("food");
+        Vector2Int tilePosition = Grid._instance.GetPosition(highestAmount);
+        Vector3 newPoitionInWorld = new Vector3(tilePosition.x, 0, tilePosition.y);
+
+        Vector3 newPositionScreen = Camera.main.WorldToScreenPoint(newPoitionInWorld);
+        if (!bottomBar.worldView)
+        {
+            currentEulerAngles = new Vector3(0, 0, 180);
+            currentRotation.eulerAngles = currentEulerAngles;
+            transform.rotation = currentRotation;
+            currentPos = positionToMoveToA;
+            moveToPos = positionToMoveToB;
+
+        }
         if (bottomBar.worldView)
         {
-            positionToMoveToA.position = new Vector3(tilePosition.x, tilePosition.y, 1f);
-            positionToMoveToB.position = new Vector3(tilePosition.x - 3f, tilePosition.y, 1f);
+            currentEulerAngles = new Vector3(0, 0, 90);
+            currentRotation.eulerAngles = currentEulerAngles;
+            transform.rotation = currentRotation;
+
+            UnityEngine.Debug.LogError("tile position is vector3:  " + newPositionScreen + "!" + newPoitionInWorld + " | " + tilePosition + " | " + highestAmount);
+            currentPos.position = newPositionScreen + new Vector3(35, 0, 0);
+            moveToPos.position = newPositionScreen + new Vector3(50, 0, 0);
         }
+
 
 
     }
