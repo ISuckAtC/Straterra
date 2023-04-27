4656e51053d47e173838579d86f3afa8c5c050f7
Rune Nohr <77842104+JeyesElite@users.noreply.github.com>
2023-04-27 14:31:57 +0200

    missions

---
+++ Assets/_Scripts/Clientside/Mission.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Mission", menuName = "Mission")]
+public class Mission : ScriptableObject
+{
+    public enum type
+    {
+        resourceAmount,
+        resourceGain,
+        townBuilding,
+        worldBuilding,
+        training,
+        raiding,
+        defending,
+        loyalty,
+        playtime
+    }
+
+    public type Type;
+
+    public int unitIdentifier;
+    public int amount;
+    
+    [Space(10)]
+    [Header("Rewards")]
+    public int foodReward;
+    public int woodReward;
+    public int metalReward;
+    public int orderReward;
+    [Space(10)]
+    public int archerReward;
+    public int cavalryReward;
+    public int swordsmanReward;
+    public int spearmanReward;
+
+
+}
